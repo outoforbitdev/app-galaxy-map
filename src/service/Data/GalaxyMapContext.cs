5a82945bf1e68b62e9c5115e17fbe9b0b023e625
Jay Mirecki <jarett.mirecki@gmail.com>
2025-02-20 20:28:45 -0600

    chore: lint

--- src/service/Data/GalaxyMapContext.cs
+++ src/service/Data/GalaxyMapContext.cs
@@ -22,61 +22,6 @@ public class GalaxyMapContext : DbContext {
             .WithOne(g => g.ChildGovernment)
             .HasForeignKey(g => g.ChildGovernmentId)
             .IsRequired();
-    //     #region System
-    //     modelBuilder.Entity<Models.System>()
-    //         .HasKey(s => s.Name);
-    //     #endregion System
-    //     #region Spacelane
-    //     modelBuilder.Entity<Models.Spacelane>()
-    //         .HasOne(s => s.Origin)
-    //         .WithMany()
-    //         .HasForeignKey(s => s.OriginId)
-    //         .IsRequired();
-    //     modelBuilder.Entity<Models.Spacelane>()
-    //         .HasOne(s => s.Destination)
-    //         .WithMany()
-    //         .HasForeignKey(s => s.DestinationId)
-    //         .IsRequired();
-    //     modelBuilder.Entity<Models.Spacelane>()
-    //         .HasNoKey();
-    //     #endregion Spacelane
-    //     #region Planet
-    //     modelBuilder.Entity<Models.Planet>()
-    //         .HasKey(p => p.Name);
-    //     modelBuilder.Entity<Models.Planet>()
-    //         .HasMany(p => p.ParentGovernments)
-    //         .WithOne(p => p.Planet)
-    //         .HasForeignKey(p => p.PlanetId)
-    //         .IsRequired();
-    //     #endregion Planet
-    //     #region Government
-    //     modelBuilder.Entity<Models.Government>()
-    //         .HasKey(g => g.Name);
-    //     modelBuilder.Entity<Models.Government>()
-    //         .Property(g => g.Color)
-    //             .HasConversion(
-    //                 v => v.ToString(),
-    //                 v => (MapColor)Enum.Parse(typeof(MapColor), v));
-    //     #endregion Government
-    //     #region GovernmentGovernment
-    //     modelBuilder.Entity<Models.GovernmentGovernment>()
-    //         .HasOne(g => g.ChildGovernment)
-    //         .WithMany()
-    //         .HasForeignKey(g => g.ChildGovernmentId)
-    //         .IsRequired();
-    //     modelBuilder.Entity<Models.GovernmentGovernment>()
-    //         .HasOne(g => g.ParentGovernment)
-    //         .WithMany()
-    //         .HasForeignKey(g => g.ParentGovernmentId)
-    //         .IsRequired();
-    //     modelBuilder.Entity<Models.GovernmentGovernment>()
-    //         .HasNoKey()
-    //         .Property(g => g.Relationship)
-    //             .HasConversion(
-    //                 v => v.ToString(),
-    //                 v => (GovernmentRelationship)Enum.Parse(typeof(GovernmentRelationship), v));
-    //     #endregion GovernmentGovernment
-    //     #region PlanetGovernment
     //     modelBuilder.Entity<Models.PlanetGovernment>()
     //         .HasOne(p => p.Government)
     //         .WithMany()
