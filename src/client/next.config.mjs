0ac15724d875bfa043d80d1d8725000730d71aa7
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2025-03-08 14:33:01 -0600

    feat: frontend support for multiple instances
    * feat: frontend support for multiple instances
    
    * chore: cleanup unused code
    
    * chore: remove unused code
    
    * feat: use X icon to close responsive menu

--- src/client/next.config.mjs
+++ src/client/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/map-legends",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
