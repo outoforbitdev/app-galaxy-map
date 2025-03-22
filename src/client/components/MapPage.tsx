06c3b2e27623b0aede4e73f1ed12e08bfb122c44
Jay Mirecki <jarett.mirecki@gmail.com>
2025-03-22 07:25:07 -0500

    chore: lint

--- src/client/components/MapPage.tsx
+++ src/client/components/MapPage.tsx
@@ -21,10 +21,10 @@ export default function MapPage(props: IMapPageProps) {
     const json_data = await data.json();
     const planet_list = json_data.systems;
     const spacelane_list = json_data.spacelanes;
-  
+
     setPlanets(planet_list);
     setSpacelanes(spacelane_list);
-  }
+  };
   useEffect(() => {
     fetchMap();
   }, []);
@@ -43,7 +43,7 @@ export default function MapPage(props: IMapPageProps) {
     hideSpacelaneLabels: true,
     showAllPlanets: true,
     customOptions: customOptions,
-  }
+  };
 
   return (
     <Fragment>
@@ -51,7 +51,7 @@ export default function MapPage(props: IMapPageProps) {
         planets={planets}
         spacelanes={spacelanes}
         dimensions={{ minX: -12000, maxX: 12000, minY: -12000, maxY: 12000 }}
-        zoom={{ initial: .5, min: .3, max: 10 }}
+        zoom={{ initial: 0.5, min: 0.3, max: 10 }}
         mapOptions={mapOptions}
       />
       <p>
