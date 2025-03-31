8122dcf03b3ad98b69df3e32b03b839b6353a950
Jay Mirecki <jarett.mirecki@gmail.com>
2025-03-30 20:32:56 -0500

    docs: add comments to all Calendar functions

--- src/service/Models/Calendar.cs
+++ src/service/Models/Calendar.cs
@@ -1,13 +1,8 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
-using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 
 namespace GalaxyMapSiteApi.Models;
 
-public struct Month {
-    public string Name;
-    public int Length;
-}
 public enum DateFormat {
     YearOnly,
     YearColonDay,
@@ -25,8 +20,6 @@ public class Calendar: InstanceEntity {
     public required bool ZeroYearAfterEpoch { get; set; }
     public string? AfterEpochSuffix { get; set; }
     public string? BeforeEpochSuffix { get; set; }
-    [NotMapped]
-    public List<Month> Months { get; set; } = new List<Month>();
     #endregion Properties
     #region Constructors
     [SetsRequiredMembers]
@@ -46,26 +39,48 @@ public class Calendar: InstanceEntity {
     #endregion Constructors
     #region Methods
     #region Conversions
+    /// <summary>
+    /// Convert a DateTime to a Date
+    /// </summary>
+    /// <param name="dateTime">The DateTime to convert</param>
+    /// <returns>Date equivalent of the provided DateTime</returns>
+    /// <exception cref="Exception">Thrown if the DateTime is too large to be converted to a Date</exception>
     public Date GetDate(DateTime dateTime) {
-        long days = dateTime.Minutes / MinutesPerHour / HoursPerDay;
-        if (days > (long)Date.Max || days < (long)Date.Min) {
-            throw new Exception($"DateTime {dateTime.Minutes} is equivalent to Date {days} is greater than largest possible date value");
+        if (!TryGetDate(dateTime, out Date date)) {
+            throw new Exception($"DateTime {dateTime.Minutes} is equivalent to Date {dateTime.Minutes / MinutesPerDay} which is a greater magnitude than allowed for a date value");
         }
-        return new Date((int)(dateTime.Minutes / MinutesPerHour / HoursPerDay));
+        return date;
     }
+    /// <summary>
+    /// Attempt to convert a DateTime to a Date
+    /// </summary>
+    /// <param name="dateTime">The DateTime to convert</param>
+    /// <param name="date">The converted Date (if successful)</param>
+    /// <returns>True if the DateTime was converted, false otherwise</returns>
     public bool TryGetDate(DateTime dateTime, out Date date) {
-        try {
-            date = GetDate(dateTime);
-            return true;
-        } catch {}
-        date = new Date();
-        return false;
+        long days = dateTime.Minutes / MinutesPerDay;
+        if (days > (long)Date.Max || days < (long)Date.Min) {
+            date = new Date();
+            return false;
+        }
+        date = new Date((int)days);
+        return true;
     }
+    /// <summary>
+    /// Convert a Date to a DateTime
+    /// </summary>
+    /// <param name="date">The Date to convert</param>
+    /// <returns>The DateTime equivalent to the provided Date</returns>
     public DateTime GetDateTime(Date date) {
-        return new DateTime((long)date.Days * HoursPerDay * MinutesPerHour);
+        return new DateTime((long)date.Days * MinutesPerDay);
     }
     #endregion Conversions
     #region GetValues
+    /// <summary>
+    /// Get the year represented by the provided Date
+    /// </summary>
+    /// <param name="date">Date value to retrieve the year from</param>
+    /// <returns>Year before or after epoch</returns>
     public int GetYear(Date date) {
         int year = GetYearWithZeroYear(date);
         if (year >= 0 && !ZeroYearAfterEpoch) {
@@ -73,6 +88,11 @@ public class Calendar: InstanceEntity {
         }
         return year;
     }
+    /// <summary>
+    /// Get the year represented by the provided DateTime
+    /// </summary>
+    /// <param name="dateTime">DateTime value to retrieve the year from</param>
+    /// <returns>Year before or after epoch</returns>
     public long GetYear(DateTime dateTime) {
         long year = GetYearWithZeroYear(dateTime);
         if (year >= 0 && !ZeroYearAfterEpoch) {
@@ -80,37 +100,71 @@ public class Calendar: InstanceEntity {
         }
         return year;
     }
+    /// <summary>
+    /// Get the year represented by the provided Date, including a Zero year
+    /// </summary>
+    /// <param name="date">Date value to retrieve the year from</param>
+    /// <returns>Year before or after epoch</returns>
     private int GetYearWithZeroYear(Date date) {
         long days = (long)date.Days - Epoch;
         int year = (int)(days / DaysPerYear);
+        // If the days value is before the epoch, and not the first day of the year, it should be rounded down to the start of the year
         if (days < 0 && days % DaysPerYear != 0) {
             year = year - 1;
         }
         return year;
     }
+    /// <summary>
+    /// Get the year represented by the provided DateTime, including a Zero year
+    /// </summary>
+    /// <param name="dateTime">DateTime value to retrieve the year from</param>
+    /// <returns>Year before or after epoch</returns>
     private long GetYearWithZeroYear(DateTime dateTime) {
         long days = dateTime.Minutes / MinutesPerDay - Epoch;
         long year = days / DaysPerYear;
+        // If the days value is before the epoch, and not the first day of the year, it should be rounded down to the start of the year
         if (days < 0 && days % DaysPerYear != 0) {
             year = year - 1;
         }
         return year;
     }
+    /// <summary>
+    /// Get the Date value of the first day of the year represented by the provided Date
+    /// </summary>
+    /// <param name="date">The Date value to interpret</param>
+    /// <returns>An integer representing the Date value of the first day of the year</returns>
     private int GetYearInDays(Date date) {
         return GetYearWithZeroYear(date) * DaysPerYear + Epoch;
     }
+    /// <summary>
+    /// Get the Date value of the first day of the year represented by the provided DateTime
+    /// </summary>
+    /// <param name="dateTime">The DateTime value to interpret</param>
+    /// <returns>An integer representing the DateTime value of the first day of the year</returns>
     private long GetYearInDays(DateTime dateTime) {
         return GetYearWithZeroYear(dateTime) * DaysPerYear + Epoch;
     }
-    private long GetYearInMinutes(DateTime dateTime) {
-        return GetYearWithZeroYear(dateTime) * DaysPerYear * MinutesPerDay;
-    }
+    /// <summary>
+    /// Get the day of year represented by the provided Date
+    /// </summary>
+    /// <param name="date">Date value to retrieve the year from</param>
+    /// <returns>Day of year</returns>
     public int GetDay(Date date) {
         return date.Days - GetYearInDays(date);
     }
+    /// <summary>
+    /// Get the day of year represented by the provided DateTime
+    /// </summary>
+    /// <param name="dateTime">DateTime value to retrieve the year from</param>
+    /// <returns>Day of year</returns>
     public long GetDay(DateTime dateTime) {
         return GetDayInMinutes(dateTime) / MinutesPerDay - GetYearInDays(dateTime);
     }
+    /// <summary>
+    /// Get the DateTime value of the start of the day represented by the provided DateTime
+    /// </summary>
+    /// <param name="dateTime">The DateTime to convert</param>
+    /// <returns>A long representing the DateTime of the start of the day</returns>
     private long GetDayInMinutes(DateTime dateTime) {
         long day = dateTime.Minutes / MinutesPerDay;
         if (day < 0 && dateTime.Minutes % MinutesPerDay != 0) {
@@ -124,23 +178,57 @@ public class Calendar: InstanceEntity {
     public int GetMonth(DateTime dateTime) {
         throw new NotImplementedException();
     }
+    /// <summary>
+    /// Get the hour of the day represented by the provided DateTime
+    /// </summary>
+    /// <param name="dateTime">The DateTime value to interpret</param>
+    /// <returns>The hour of the day</returns>
     public int GetHour(DateTime dateTime) {
         return (int)(dateTime.Minutes - GetDayInMinutes(dateTime)) / MinutesPerHour;
     }
+    /// <summary>
+    /// Get the minute of the hour represented by the provided DateTime
+    /// </summary>
+    /// <param name="dateTime">The DateTime value to interpret</param>
+    /// <returns>The minute of the hour</returns>
     public int GetMinute(DateTime dateTime) {
         return (int)(dateTime.Minutes - GetDayInMinutes(dateTime)) - GetHour(dateTime) * MinutesPerHour;
     }
     #endregion GetValues
     #region GetStrings
+    /// <summary>
+    /// Get a string intepretation of the provided Date
+    /// </summary>
+    /// <param name="date">The Date to interpret</param>
+    /// <param name="format">The DateFormat to use for the string</param>
+    /// <returns>A string representing the provided Date</returns>
     public string GetDateString(Date date, DateFormat format) {
         return GetDateString(GetYear(date), GetDay(date), format);
     }
+    /// <summary>
+    /// Get a string intepretation of the date represented by the provided DateTime
+    /// </summary>
+    /// <param name="dateTime">The DateTime to interpret</param>
+    /// <param name="format">The DateFormat to use for the string</param>
+    /// <returns>A string representing the date of the provided DateTime</returns>
     public string GetDateString(DateTime dateTime, DateFormat format) {
         return GetDateString(GetYear(dateTime), GetDay(dateTime), format);
     }
+    /// <summary>
+    /// Get a string intepretation of the time represented by the provided DateTime
+    /// </summary>
+    /// <param name="dateTime">The DateTime to interpret</param>
+    /// <returns>A string representing the time of the provided DateTime</returns>
     public string GetTimeString(DateTime dateTime) {
         return GetTime(GetHour(dateTime), GetMinute(dateTime));
     }
+    /// <summary>
+    /// Get a string representing the provided year and day
+    /// </summary>
+    /// <param name="year">Year to use in the string</param>
+    /// <param name="day">Day of year to use in the string</param>
+    /// <param name="format">DateFormat to use for the string</param>
+    /// <returns>A string representing the provided year and day</returns>
     private string GetDateString(long year, long day, DateFormat format) {
         switch (format) {
             case DateFormat.YearOnly:
@@ -151,6 +239,12 @@ public class Calendar: InstanceEntity {
                 return GetYearColonDayString(year, day);
         }
     }
+    /// <summary>
+    /// Add the appropriate suffix to the provided string, based on the provided year
+    /// </summary>
+    /// <param name="year">The year to use for determining the suffix</param>
+    /// <param name="dateString">The string to append the suffix</param>
+    /// <returns>The provided string with the appropriate suffix</returns>
     private string AddSuffix(long year, string dateString){
         if (year >= 0 && AfterEpochSuffix is not null) {
             return $"{dateString} {AfterEpochSuffix}";
@@ -160,18 +254,35 @@ public class Calendar: InstanceEntity {
         }
         return dateString;
     }
+    /// <summary>
+    /// Get the string representing the provided year in the format "{year} {suffix}"
+    /// </summary>
+    /// <param name="year">The year to derive the string from</param>
+    /// <returns>String representing the provided year</returns>
     private string GetYearString(long year) {
         if (year < 0 && BeforeEpochSuffix is not null){
             return AddSuffix(year, $"{year *= -1}");
         }
         return AddSuffix(year, year.ToString());
     }
+    /// <summary>
+    /// Get the string representing the provided year in the format "{year}:{day} {suffix}
+    /// </summary>
+    /// <param name="year">The year from which to derive the string</param>
+    /// <param name="day">The day from which to derive the string</param>
+    /// <returns>String representing the provided year and day</returns>
     private string GetYearColonDayString(long year, long day) {
         if (year < 0 && BeforeEpochSuffix is not null){
         return AddSuffix(year, $"{year * -1}:{day}");
         }
         return AddSuffix(year, $"{year}:{day}");
     }
+    /// <summary>
+    /// Get a string representation of the provided hour and minute in the format "{hour}:{minute}"
+    /// </summary>
+    /// <param name="hour">The hour from which to derive the string</param>
+    /// <param name="minute">The minute from which to derive the string</param>
+    /// <returns>String representing of the provided hour and minute</returns>
     private string GetTime(int hour, int minute) {
         int hourDigits = 0;
         int minuteDigits = 0;
@@ -187,9 +298,21 @@ public class Calendar: InstanceEntity {
         }
         return $"{hour.ToString($"D{hourDigits}")}:{minute.ToString($"D{minuteDigits}")}";
     }
+    /// <summary>
+    /// Get a string interpretation of the date and time represented by the provided Date
+    /// </summary>
+    /// <param name="date">The Date to interpret</param>
+    /// <param name="dateFormat">The DateFormat to use for the string</param>
+    /// <returns>String representing the provided Date</returns>
     public string GetDateTimeString(Date date, DateFormat dateFormat) {
         return GetDateTimeString(GetDateTime(date), dateFormat);
     }
+    /// <summary>
+    /// Get a string interpretation of the date and time represented by the provided Datetime
+    /// </summary>
+    /// <param name="dateTime">The DateTime to interpret</param>
+    /// <param name="dateFormat">The DateFormat to use for the string</param>
+    /// <returns>String representing the provided DateTime</returns>
     public string GetDateTimeString(DateTime dateTime, DateFormat dateFormat) {
         return $"{GetDateString(dateTime, dateFormat)} {GetTimeString(dateTime)}";
     }
