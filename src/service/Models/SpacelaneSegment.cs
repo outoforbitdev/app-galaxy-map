59037bbaf3c77bc7cdbf5556420a951d0d5095fb
Jay Mirecki <jarett.mirecki@gmail.com>
2025-04-05 17:00:52 -0400

    feat: add spacelane start and end reasons

--- src/service/Models/SpacelaneSegment.cs
+++ src/service/Models/SpacelaneSegment.cs
@@ -3,6 +3,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GalaxyMapSiteApi.Models;
 
+public enum SpacelaneStartReason
+{
+    Discovered,
+    Created,
+}
+
+public enum SpacelaneEndReason
+{
+    Collapsed,
+    Lost,
+}
+
 [Table("spacelane_segments")]
 [Keyless]
 public class SpacelaneSegment : KeylessInstanceEntity
@@ -19,12 +31,40 @@ public class SpacelaneSegment : KeylessInstanceEntity
     [ForeignKey("InstanceId, DestinationId")]
     public virtual System Destination { get; set; } = null!;
     public string DestinationId { get; set; }
+
+    #region Dates
     [NotMapped]
     public Date? StartDate { get; set; }
-    public int? StartDateValue { get { return StartDate?.Days; } set { StartDate = value is not null ? new Date((int)value) : null; } }
+    public int? StartDateValue
+    {
+        get { return StartDate?.Days; }
+        set { StartDate = value is not null ? new Date((int)value) : null; }
+    }
+
     [NotMapped]
     public Date? EndDate { get; set; }
-    public int? EndDateValue { get { return EndDate?.Days; } set { EndDate = value is not null ? new Date((int)value) : null; } }
+    public int? EndDateValue
+    {
+        get { return EndDate?.Days; }
+        set { EndDate = value is not null ? new Date((int)value) : null; }
+    }
+
+    [NotMapped]
+    public SpacelaneStartReason? StartReason { get; set; }
+    public string? StartReasonString
+    {
+        get { return StartReason?.ToString(); }
+        set { StartReason = EnumConverter.ConvertToEnumOrNull<SpacelaneStartReason>(value); }
+    }
+
+    [NotMapped]
+    public SpacelaneEndReason? EndReason { get; set; }
+    public string? EndReasonString
+    {
+        get { return EndReason?.ToString(); }
+        set { EndReason = EnumConverter.ConvertToEnumOrNull<SpacelaneEndReason>(value); }
+    }
+    #endregion Dates
     #endregion Properties
 
     #region Constructors
---
+++ src/serviceTests/EnumConverterTests.cs
@@ -0,0 +1,44 @@
+﻿using GalaxyMapSiteApi.Models;
+
+namespace GalaxyMapSiteApi.Tests;
+
+public class EnumConverterTests
+{
+    [Fact]
+    public void ConvertToEnumOrDefault_ValidInput_Enum()
+    {
+        SpacelaneStartReason actual = EnumConverter.ConvertToEnumOrDefault(
+            "Created",
+            SpacelaneStartReason.Discovered
+        );
+        Assert.Equal(SpacelaneStartReason.Created, actual);
+    }
+
+    [Fact]
+    public void ConvertToEnumOrDefault_InvalidInput_Default()
+    {
+        SpacelaneStartReason actual = EnumConverter.ConvertToEnumOrDefault(
+            "not a real reason",
+            SpacelaneStartReason.Discovered
+        );
+        Assert.Equal(SpacelaneStartReason.Discovered, actual);
+    }
+
+    [Fact]
+    public void ConvertToEnumOrNull_ValidInput_Enum()
+    {
+        SpacelaneStartReason? actual = EnumConverter.ConvertToEnumOrNull<SpacelaneStartReason>(
+            "Created"
+        );
+        Assert.Equal(SpacelaneStartReason.Created, actual);
+    }
+
+    [Fact]
+    public void ConvertToEnumOrNull_InvalidInput_Null()
+    {
+        SpacelaneStartReason? actual = EnumConverter.ConvertToEnumOrNull<SpacelaneStartReason>(
+            "not a real reason"
+        );
+        Assert.Null(actual);
+    }
+}
