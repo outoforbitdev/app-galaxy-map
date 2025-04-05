6d4aafe490122a311c3a011df584bfe8a717f150
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2025-04-05 16:24:27 -0400

    docs: update About page (#175)
    * docs: update About page
    
    * docs: remove contractions
    
    * docs: improve language for the About page

--- src/client/app/about/page.tsx
+++ src/client/app/about/page.tsx
@@ -1,42 +1,60 @@
 export default function About() {
   return (
     <div>
-      <h3>What this is</h3>
+      <h3>What this is?</h3>
       <p>
         This is a map of the systems in the Star Wars galaxy and the hyperlanes
-        that connect them. It’s meant as a way to explore the galaxy.
+        that connect them. It is designed as a way to explore the galaxy and
+        better understand how everything fits together.
       </p>
-      <h3>Why I made this</h3>
+      <h3>Why did I make this?</h3>
       <p>
-        As I read and watch Star Wars I enjoy trying to map out the path that
-        our heroes are taking and the territory that the factions are fighting
-        over. There’s no shortage of maps out there that all provide their own
-        benefits, but there are none that I feel are feature-complete. This is
-        my attempt to make a detailed map with all the features I always wanted.
+        As I read and watch Star Wars, I enjoy trying to map out the paths our
+        heroes take and the territories various factions control. While there
+        are plenty of maps out there, each with their own strengths, I have
+        never found one that feels feature-complete. This is my attempt to build
+        the detailed map I have always wanted.
       </p>
-      <h3>How I made this</h3>
+      <h3>What is the difference between the Legends and the Legends+ maps?</h3>
       <p>
-        I gathered the initial data for this map from Star Wars: The Essential
-        Atlas and Gimp to map the pixel locations of each mapped system. I used
-        Wookiepedia to compile all the hyperlanes in the galaxy.
+        For the Legends map, I have stuck as closely as possible to data
+        confirmed by sources from the Legends continuity. That means out of the
+        5,000+ star systems in Legends, fewer than 3,000 are actually mapped,
+        and under 2,000 are connected by hyperlanes.
+      </p>
+      <p>
+        The Legends+ map includes additional data that I believe fits within the
+        context of Legends, even if it has not been explicitly confirmed. For
+        example, I have placed the planet Lorell between Andalia and Zeltros,
+        and extended the Lorell Route to connect all three systems.
+      </p>
+      <h3>How did I make this?</h3>
+      <p>
+        I started by gathering data from Star Wars: The Essential Atlas. I
+        imported each map—from galactic-scale to detailed sector-level—into GIMP
+        to determine the pixel coordinates of each system and calculate their
+        galactic positions.
+      </p>
+      <p>
+        I used Wookieepedia to compile information on the galaxy’s hyperlanes,
+        confirming each one using the cited sources whenever possible.
+      </p>
+      <p>
+        The UI was built from the ground up as an SVG in React using Next.js.
+        The server runs on ASP.NET Core and connects to a SQL database.
       </p>
       <p>
         The UI for this was built from the ground up as an svg in React using
         Next.js.
       </p>
-      <h3>Planned features</h3>
-      <ul>
-        <li>Zoom</li>
-        <li>Drag to move</li>
-        <li>
-          Versions:
-          <ul>
-            <li>Legends Clone Wars</li>
-            <li>Legends First Galactic Civil War</li>
-            <li>Legends Second Galactic Civil War</li>
-          </ul>
-        </li>
-      </ul>
+      <h3>What is next?</h3>
+      <p>
+        So far, I have mapped over 2,000 systems, but nearly 3,000 more remain
+        unmapped in both the Legends and Legends+ versions. I will probably
+        always be adding more as I discover new information. The next major
+        feature will be the ability to view the government affiliations of each
+        system at various timepoints—starting with the Clone Wars.
+      </p>
     </div>
   );
 }
